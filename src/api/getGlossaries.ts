58339de8ed7d6780e1614d5e0fe44a18a9956ece
igeligel <kevinigeligeligel@gmail.com>
2021-06-25 14:15:01 +0200

    fix: glossaries
    fix if no cookie available

--- src/api/getGlossaries.ts
+++ src/api/getGlossaries.ts
@@ -8,13 +8,17 @@ type GetGlossariesProps = {
 export const getGlossaries = async (input: GetGlossariesProps) => {
   const { cookies } = input;
 
+  const tooltiprAntiCsrfTokenCookie = cookies.find(
+    (cookie) => cookie.name === "tooltipr_sAntiCsrfToken"
+  );
+
+  if (!tooltiprAntiCsrfTokenCookie) return { data: { success: false } };
+
   const dictionaryResponse = await axios.get<any>(
     `${Configuration.DOMAIN}/api/extension/dictionaries`,
     {
       headers: {
-        "anti-csrf": cookies.find(
-          (cookie) => cookie.name === "tooltipr_sAntiCsrfToken"
-        ).value,
+        "anti-csrf": tooltiprAntiCsrfTokenCookie.value,
         Cookie: cookies
           .map((cookie) => `${cookie.name}=${cookie.value}`)
           .join("; "),
