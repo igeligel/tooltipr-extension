9d63e88dba0c506c8e6c78d31a5c9987ef6e304f
igeligel <kevinigeligeligel@gmail.com>
2021-07-14 16:05:32 +0200

    feat: filter
    more style, also lets trigger people

--- src/pages/UrlFiltering/index.tsx
+++ src/pages/UrlFiltering/index.tsx
@@ -1,14 +1,8 @@
-import {
-  Box,
-  Button,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Icon,
-} from "@chakra-ui/react";
+import { Box, Button, Icon, Text } from "@chakra-ui/react";
 import React, { forwardRef, useEffect, useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import { useHistory } from "react-router-dom";
+import { IllustrationFilter } from "../../components/IllustrationFilter";
 
 export const UrlFiltering = forwardRef((props, ref) => {
   const history = useHistory();
@@ -48,20 +42,43 @@ export const UrlFiltering = forwardRef((props, ref) => {
           <Button
             size={"xs"}
             leftIcon={<Icon as={FiPlus} />}
-            colorScheme="teal"
+            colorScheme="cyan"
             variant="solid"
             width={"100%"}
+            background={"cyan.200"}
+            _hover={{ background: "cyan.300" }}
           >
             Add Filter Rule
           </Button>
         </Box>
-        <Box flex={1} display={"flex"} alignItems={"center"}>
-          <Box>dawaw</Box>
+        <Box
+          flex={1}
+          display={"flex"}
+          alignItems={"center"}
+          width={"100%"}
+          justifyContent={"center"}
+          flexDirection={"column"}
+        >
+          <IllustrationFilter width={"60%"} />
+          <Text color={"gray.400"} fontSize={"md"} fontWeight={"light"}>
+            Add a new rule and filter where to hide tooltips
+          </Text>
+          <Box marginTop={"2"}>
+            <Button
+              background={"cyan.200"}
+              _hover={{ background: "cyan.300" }}
+              colorScheme="cyan"
+              variant="solid"
+              size={"xs"}
+            >
+              Add Filter Rule
+            </Button>
+          </Box>
         </Box>
       </Box>
-      <Box flexShrink={0} display={"flex"} width={"100%"}>
+      {/* <Box flexShrink={0} display={"flex"} width={"100%"}>
         <Box>Footer</Box>
-      </Box>
+      </Box> */}
     </Box>
   );
 });
