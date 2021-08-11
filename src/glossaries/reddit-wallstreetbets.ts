6f8e63f192aaba1f97374c65433025ec7be54383
igeligel <kevinigeligeligel@gmail.com>
2021-08-11 21:23:47 +0200

    feat: wsb

---
+++ src/glossaries/reddit-wallstreetbets.ts
@@ -0,0 +1,46 @@
+import { Glossary } from "../types";
+
+const RedditWallstreetBetsTag = {
+  name: "frontend",
+  color: "blue",
+};
+
+export const RedditWallstreetBets: Glossary = {
+  uuid: "1ec940a0-74fd-47a3-ad12-ac93eaf95c6d",
+  description: "wadadwwad",
+  title: "r/wallstreetbets",
+  terms: [
+    {
+      uuid: "",
+      term: "",
+      title: "",
+      description: ` [Wikipedia]`,
+      tags: [RedditWallstreetBetsTag],
+    },
+    // $GME
+    // GME
+    // CNBC
+    // $AMC
+    // AMC
+    // u/deepfuckingvalue
+    // FUD (Fear Uncertainty Doubt)
+    // HFT
+    // BlackRock
+    // Vanguard
+    // Fidelity
+    // Schwab
+    // RobinHood
+    // PT Price Target
+    // $SPX
+    // DIAMOND HANDS
+    // 401k
+    // BB/BBBY/AMC/NOK
+    // SEC
+    // FINRA
+    // DVP Delivery Versus Payment
+    // DD = Double Down
+    // bull
+    // bear
+    //
+  ],
+};
